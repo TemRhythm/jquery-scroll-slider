ba683459cc7d26581e6dfefa81842174bc7bdf3a
Rustem Zakiev <temrhythm@gmail.com>
2014-08-02 23:50:49 +0400

    add option thumbs (false, true)

--- scrollSlider.jquery.js
+++ scrollSlider.jquery.js
@@ -6,7 +6,6 @@
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
         var base = this;
-
         // Access to jQuery and DOM versions of element
         base.$el = $(el);
         base.el = el;
@@ -21,26 +20,44 @@
             var slides = $('<div class="slides"></div>');
             scrollSliderWrapper.append(slides);
             var scrollBar = $('<div class="scroll-bar"><div class="scroll-button"></div></div>');
-            var thumbnails = $('<div class="thumbnails"></div>');
+            if(base.options.thumbs)
+                var thumbnails = $('<div class="thumbnails"></div>');
             var currentSlide;
             var currentThumb;
+            var currentThumbShadow;
             var currentImageWrapper;
             images.each(function () {
                 currentSlide = $('<div class="slide"><span class="img-middle-helper"></span></div>');
                 $(this).appendTo(currentSlide);
                 slides.append(currentSlide);
-                currentThumb = $('<div class="thumb"></div>');
-                currentImageWrapper = $('<div class="thumb-image-wrapper"></div>');
-                var thumb = $(this).clone();
-                thumb.appendTo(currentImageWrapper);
-                currentImageWrapper.appendTo(currentThumb);
-                currentThumb.appendTo(thumbnails);
-                changeThumbSize(thumb[0]);
+                if(base.options.thumbs){
+                    currentThumb = $('<div class="thumb"></div>');
+                    currentThumbShadow = $('<div class="thumb-shadow"><span></span></div>').click(function(){
+                        var slideIndex = thumbnails.children().index($(this).parent());
+                        firstSlide.animate({
+                            "margin-top": (-slideHeight*slideIndex) + 'px'
+                        }, 200);
+                    });
+                    currentThumb.append(currentThumbShadow);
+                    currentImageWrapper = $('<div class="thumb-image-wrapper"></div>');
+                    var thumb = $(this).clone();
+                    thumb.appendTo(currentImageWrapper);
+                    currentImageWrapper.appendTo(currentThumb);
+                    currentThumb.appendTo(thumbnails);
+                    changeThumbSize(thumb[0]);
+                }
             });
             base.$el.append(scrollSliderWrapper);
             base.$el.append(scrollBar);
-            base.$el.append(thumbnails);
+            if(base.options.thumbs)
+                base.$el.append(thumbnails);
 
+            base.$el.find('.scroll-button').draggable({
+                axis: "y",
+                containment: "#"+base.$el.attr('id')+" .scroll-bar",
+                drag: checkSlide,
+                stop: checkSlide
+            });
             var firstSlide = base.$el.find('.slides .slide:first-child');
             var firstSlideMarginTop = parseInt(firstSlide.css('margin-top'));
             var firstThumb = base.$el.find('.thumbnails .thumb:first-child');
@@ -50,12 +67,6 @@
             var scrollStepHeight = base.$el.find('.scroll-bar').innerHeight() / (images.length + 2);
             var currentSlideNumber = 1;
             var lastSlidePos = 0;
-            base.$el.find('.scroll-button').draggable({
-                axis: "y",
-                containment: "#"+base.$el.attr('id')+" .scroll-bar",
-                drag: checkSlide,
-                stop: checkSlide
-            });
             function checkSlide() {
                 var scrollPosition = base.$el.find('.scroll-button').position().top - parseInt(base.$el.find('.scroll-bar').css('padding-top'));
                 if (scrollPosition > lastSlidePos && scrollPosition > scrollStepHeight * (currentSlideNumber - 1))
@@ -96,9 +107,9 @@
                 if(img.width == 0)
                     return;
                 if (img.width > img.height)
-                    img.height = options.thumbSize;
+                    img.height = base.options.thumbSize;
                 else
-                    img.width = options.thumbSize;
+                    img.width = base.options.thumbSize;
             }
         };
         // Sample Function, Uncomment to use
@@ -112,7 +123,8 @@
 
     $.scrollSlider.defaultOptions = {
         radius: "20px",
-        thumbSize : 100
+        thumbSize : 100,
+        thumbs: true
     };
 
     $.fn.scrollSlider = function(options){
